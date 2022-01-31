d1ab0952e622252eac657d6e84d452e9a127955c
Henrique Doro <opensource@hdoro.dev>
2022-01-31 18:43:05 -0300

    chore: upgrade dependencies

--- template/lib/sanity.js
+++ template/lib/sanity.js
@@ -1,5 +1,4 @@
 import {
-  groq,
   createClient,
   createImageUrlBuilder,
   createPreviewSubscriptionHook,
@@ -9,6 +8,7 @@ const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   useCdn: process.env.NODE_ENV === 'production',
+  apiVersion: '2022-01-31'
 }
 
 export const imageBuilder = source => createImageUrlBuilder(config).image(source)
