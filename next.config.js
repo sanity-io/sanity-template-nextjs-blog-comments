b49d9a7d1b7074c68e2b637712f6f465ae567c4b
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-08-27 11:09:43 +0200

    chore: future proofing

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  images: {
+    domains: ['cdn.sanity.io', 'source.unsplash.com'],
+  },
+}
+
+module.exports = nextConfig
